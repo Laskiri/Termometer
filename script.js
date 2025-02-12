b27d3c7e1308384afaab2c1a07200421a86de995
Lasse Drongesen <Lassemgp@gmail.com>
2025-02-12 18:21:15 +0100

    updated to use drive assets instead

--- script.js
+++ script.js
@@ -7,52 +7,6 @@ let graderFahrenheit = document.getElementById('graderFahrenheit');
 let graderInput = document.getElementById('graderInput');
 let emojiElement = document.getElementById('expression');
 
-function getEmojiPath(temp) {
-    const assetsPath = './assets/';
-    let path = assetsPath + 'earth.png';
-
-    if (temp >= -273 && temp < -100) {
-        path = assetsPath + 'XXCold.png';
-    }
-    else if (temp >= -100 && temp < -50) {
-        path = assetsPath + 'XCold.png';
-    }
-
-    else if (temp >= -50 && temp < 0) {
-        path = assetsPath + 'MCold.png';
-    }
-
-    else if (temp >= 0 && temp < 15) {
-        path = assetsPath + 'Cold.png';
-    }
-
-    else if (temp >= 15 && temp < 30) {
-        path = assetsPath + 'SHot.png';
-    }
-
-    else if (temp >= 30 && temp < 50) {
-        path = assetsPath + 'MHot.png';
-    }
-
-    else if (temp >= 50 && temp < 100) {
-        path = assetsPath + 'Hot.png';
-    }
-
-    else if (temp >= 100 && temp < 200) {
-        path = assetsPath + 'XHot.png';
-    }
-
-    else if (temp >= 200) {
-        path = assetsPath + 'XXHot.png';
-    }
-    else {
-        console.log("Error: " + temp);
-    }
-    return path;
-}
-
-
-
 function showSliderValue(grader) {
     const Fah = grader * 1.8 + 32;
 
@@ -75,7 +29,6 @@ showSliderValue(0);
 // Add separate event listener for the slider (range) input
 slider_input.addEventListener('input', () => {
     const temp = slider_input.value - 273;
-    console.log("slider: " + temp);
     const emoji = getEmojiPath(temp);
     emojiElement.src = emoji;
     showSliderValue(temp);
@@ -84,7 +37,45 @@ slider_input.addEventListener('input', () => {
 // Add separate event listener for the text input
 graderInput.addEventListener('input', () => {
     const temp = parseInt(graderInput.value);
-    console.log("text: " + temp);
+    const emoji = getEmojiPath(temp);
     emojiElement.src = emoji;
     showSliderValue(temp);
 });
+
+function getEmojiPath(temp) {
+    let path = '';
+    if (temp >= -273 && temp < -100) {
+        path = "https://drive.google.com/thumbnail?id=1PmLmen7ZCDgE_hVBzubqb7V7WZ-021Zq&sz=s800";
+    }
+    else if (temp >= -100 && temp < -50) {
+        path = "https://drive.google.com/thumbnail?id=1xpT-wz2UiAVBlmfKJqVLVxs3vuFJ40K6&sz=s800";
+    }
+
+    else if (temp >= -50 && temp < 0) {
+        path = "https://drive.google.com/thumbnail?id=1Tb33xGYTrOzWAQLYlJio3-zPF_o2f72O&sz=s800";
+    }
+    else if (temp >= 0 && temp < 15) {
+        path = "https://drive.google.com/thumbnail?id=1ln6N2STqCkgic_KeY6I_39KFN0N9bQWB&sz=s800";
+    }
+
+    else if (temp >= 15 && temp < 30) {
+        path = "https://drive.google.com/thumbnail?id=1f-0LOPlePHvmDhjoe44buStk-BQNscIT&sz=s800";
+    }
+
+    else if (temp >= 30 && temp < 50) {
+        path = "https://drive.google.com/thumbnail?id=1n3eeiOSytByT_U2WnUP2T5x-SeJ8U-9T&sz=s800";
+    }
+    else if (temp >= 50 && temp < 100) {
+        path = "https://drive.google.com/thumbnail?id=1o1EScHWB3-lNNjqvbYQLSW3SGQEn51BC&sz=s800";
+    }
+    else if (temp >= 100 && temp < 200) {
+        path = "https://drive.google.com/thumbnail?id=1shxAFtUHzO-qbkEzE1l-mOrWyU6w_1hC&sz=s800";
+    }
+    else if (temp >= 200) {
+        path = "https://drive.google.com/thumbnail?id=1CRBUMNLdM81MHnVvxYeWFaobfyKuih3z&sz=s800";
+    }
+    else {
+        console.log("Error: " + temp);
+    }
+    return path;
+}
